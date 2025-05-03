889575635a5fc9f1336aefa6eb1b1acd2c94ad3e
Neeraj177 <neerajyt17@gmail.com>
2025-05-03 09:17:31 +0530

    day -2 about optional and parameter handling

--- src/main/java/com/example/demo/controller/Controller.java
+++ src/main/java/com/example/demo/controller/Controller.java
@@ -1,9 +1,6 @@
 package com.example.demo.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/test")
@@ -12,5 +9,29 @@ public class Controller {
     public String login( )
     {
         return "hello";
+    }
+
+    @RequestMapping("/getid")
+    String getData(@RequestParam(value = "un" , required = false) String uname )
+    {
+
+        return uname;
+    }
+
+    @RequestMapping("/getuser/{id}")
+    String getDataById(@PathVariable int  id )
+    {
+        int x = id;
+        switch(x) {
+            case 1 -> System.out.println("user 1");
+            case 2 ->System.out.println("user 2 ");
+            case 3 ->System.out.println("user 3 ");
+            case 4 ->System.out.println("user 4 ");
+            case 5 ->System.out.println("user 5 ");
+            case 6 ->System.out.println("user 6 ");
+            case 7 ->System.out.println("user 7 ");
+            case 8 ->System.out.println("user 8 ");
+        }
+        return "user - >"+id;
     }
 }
